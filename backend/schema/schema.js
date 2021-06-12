41635dc7d2bd59b4a31fd822af1137f3124172e8
Diganta <ghoshdiganta226@gmail.com>
2021-06-13 01:33:47 +0530

    Relations added

--- backend/schema/schema.js
+++ backend/schema/schema.js
@@ -1,11 +1,11 @@
 const graphql = require("graphql")
 const _ = require("lodash")
 
-const {GraphQLID,GraphQLInt,GraphQLObjectType,GraphQLString,GraphQLSchema} = graphql
+const {GraphQLID,GraphQLInt,GraphQLObjectType,GraphQLString,GraphQLSchema,GraphQLList} = graphql
 
 var Books = [
-    {id: "1",name: "3 idots",genre: "action"},
-    {id: "2",name: "PK",genre: "romantic"}
+    {id: "1",name: "3 idots",genre: "action",authorid: "1"},
+    {id: "2",name: "PK",genre: "romantic",authorid: "2"}
 ]
 
 var authors = [
@@ -18,7 +18,13 @@ const Book = new GraphQLObjectType({
     fields: () => ({
         id: {type: GraphQLID},
         name: {type: GraphQLString},
-        genre: {type: GraphQLString}
+        genre: {type: GraphQLString},
+        author: {
+            type: author,
+            resolve(parent,args){
+                return _.find(authors,{id: parent.authorid})
+            }
+        }
     })
 })
 
@@ -26,7 +32,13 @@ const author = new GraphQLObjectType({
     name: "Author",
     fields: () => ({
         id: {type: GraphQLID},
-        name: {type: GraphQLString}
+        name: {type: GraphQLString},
+        books: {
+            type: new GraphQLList(Book),
+            resolve(parent,args){
+                return _.filter(Books,{authorid: parent.id})
+            }
+        }
     })
 })
 
